b4c0f7314e0e8323b3cdd557492b8d8eb8df2aca
DyingFlower <arthurvieiradelacerda@gmail.com>
2025-06-16 19:32:52 -0300

    homepage

--- initialpage/homepage.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("login_button");
-
-function redirecttopage() {
-    window.location.href = "loginpage.html";
-}
-button.addEventListener("click", redirecttopage);
