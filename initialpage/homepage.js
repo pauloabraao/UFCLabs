2b439da0eb87e42f856506af5be0ba6aa60e55e7
DyingFlower <arthurvieiradelacerda@gmail.com>
2025-06-16 18:56:23 -0300

    Adiciona página inicial com assets   - HTML, CSS e JS da homepage   - Imagens (logo, curvas)

---
+++ initialpage/homepage.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("login_button");
+
+function redirecttopage() {
+    window.location.href = "loginpage.html";
+}
+button.addEventListener("click", redirecttopage);
