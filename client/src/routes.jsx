3222aa9dade11836d0e06c273e4099ac5dabc962
pauloabraao <abraaoteles@alu.ufc.br>
2025-07-20 22:08:54 -0300

    Added LabsPage

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,11 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import ReportPage from './pages/ReportPage';
+import LabsPage from './pages/LabsPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
   },
+  {
+    path: '/report',
+    element: <ReportPage/>,
+  },
+  {
+    path: '/labs',
+    element: <LabsPage />,
+  }
 ]);
 
 export default router;
