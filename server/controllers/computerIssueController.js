5bbe73faa2a9e76dba0dc8abe1639e976973f6a9
igcozmu <igoremanuel28@gmail.com>
2025-07-24 15:28:59 -0300

    Update computerIssueController

--- server/controllers/computerIssueController.js
+++ server/controllers/computerIssueController.js
@@ -1,6 +1,25 @@
-const ComputerIssue = require('../models/ComputerIssue');
+import ComputerIssue from '../models/ComputerIssue.js';
 
-exports.getAllComputerIssues = async (req, res) => {
+/**
+ * @swagger
+ * /api/computer-issues:
+ *   get:
+ *     summary: Listar todos os problemas de computadores
+ *     description: Retorna uma lista com todos os problemas reportados nos computadores
+ *     tags: [Computer Issues]
+ *     responses:
+ *       200:
+ *         description: Lista de problemas recuperada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/ComputerIssue'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
+export const getAllComputerIssues = async (req, res) => {
   try {
     const issues = await ComputerIssue.findAll();
     res.json(issues);
@@ -9,7 +28,61 @@ exports.getAllComputerIssues = async (req, res) => {
   }
 };
 
-exports.createComputerIssue = async (req, res) => {
+/**
+ * @swagger
+ * /api/computer-issues:
+ *   post:
+ *     summary: Reportar um novo problema
+ *     description: Cria um novo reporte de problema em um computador
+ *     tags: [Computer Issues]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required: [computer_id, reported_by, description, date_reported, status, component]
+ *             properties:
+ *               computer_id:
+ *                 type: integer
+ *                 description: ID do computador com problema
+ *                 example: 1
+ *               reported_by:
+ *                 type: integer
+ *                 description: ID do usuário que reportou
+ *                 example: 1
+ *               description:
+ *                 type: string
+ *                 description: Descrição do problema
+ *                 example: Computador não liga
+ *               date_reported:
+ *                 type: string
+ *                 format: date
+ *                 description: Data que o problema foi reportado
+ *                 example: "2025-01-20"
+ *               status:
+ *                 type: string
+ *                 enum: [reportado, em analise, resolvido, fechado]
+ *                 description: Status do problema
+ *                 example: reportado
+ *               component:
+ *                 type: string
+ *                 enum: [hardware, software, rede]
+ *                 description: Componente com problema
+ *                 example: hardware
+ *     responses:
+ *       201:
+ *         description: Problema reportado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ComputerIssue'
+ *       400:
+ *         $ref: '#/components/responses/BadRequest'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
+export const createComputerIssue = async (req, res) => {
   try {
     const { computer_id, reported_by, description, date_reported, status, component } = req.body;
     const newIssue = await ComputerIssue.create({
@@ -21,7 +94,34 @@ exports.createComputerIssue = async (req, res) => {
   }
 };
 
-exports.getComputerIssueById = async (req, res) => {
+/**
+ * @swagger
+ * /api/computer-issues/{id}:
+ *   get:
+ *     summary: Buscar problema por ID
+ *     description: Retorna um problema específico pelo seu ID
+ *     tags: [Computer Issues]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID único do problema
+ *         example: 1
+ *     responses:
+ *       200:
+ *         description: Problema encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ComputerIssue'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
+export const getComputerIssueById = async (req, res) => {
   try {
     const issue = await ComputerIssue.findByPk(req.params.id);
     if (!issue) {
@@ -33,7 +133,7 @@ exports.getComputerIssueById = async (req, res) => {
   }
 };
 
-exports.updateComputerIssue = async (req, res) => {
+export const updateComputerIssue = async (req, res) => {
   try {
     const { computer_id, reported_by, description, date_reported, status, component } = req.body;
     const issue = await ComputerIssue.findByPk(req.params.id);
@@ -47,7 +147,7 @@ exports.updateComputerIssue = async (req, res) => {
   }
 };
 
-exports.deleteComputerIssue = async (req, res) => {
+export const deleteComputerIssue = async (req, res) => {
   try {
     const issue = await ComputerIssue.findByPk(req.params.id);
     if (!issue) {
