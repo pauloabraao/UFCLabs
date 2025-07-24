1ebb6fa90f371f69cd60148f40d841b4fb79ca8c
igcozmu <igoremanuel28@gmail.com>
2025-07-24 15:34:32 -0300

    Update programController

--- server/controllers/programController.js
+++ server/controllers/programController.js
@@ -1,6 +1,25 @@
-const Program = require('../models/Program');
+import Program from '../models/Program.js';
 
-exports.getAllPrograms = async (req, res) => {
+/**
+ * @swagger
+ * /api/programs:
+ *   get:
+ *     summary: Listar todos os programas
+ *     description: Retorna uma lista com todos os programas de software cadastrados
+ *     tags: [Programs]
+ *     responses:
+ *       200:
+ *         description: Lista de programas recuperada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Program'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
+export const getAllPrograms = async (req, res) => {
   try {
     const programs = await Program.findAll();
     res.json(programs);
@@ -9,7 +28,44 @@ exports.getAllPrograms = async (req, res) => {
   }
 };
 
-exports.createProgram = async (req, res) => {
+/**
+ * @swagger
+ * /api/programs:
+ *   post:
+ *     summary: Criar um novo programa
+ *     description: Adiciona um novo programa de software ao sistema
+ *     tags: [Programs]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required: [name, version]
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 maxLength: 100
+ *                 description: Nome do programa
+ *                 example: Visual Studio Code
+ *               version:
+ *                 type: string
+ *                 maxLength: 50
+ *                 description: Versão do programa
+ *                 example: 1.85.0
+ *     responses:
+ *       201:
+ *         description: Programa criado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Program'
+ *       400:
+ *         $ref: '#/components/responses/BadRequest'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
+export const createProgram = async (req, res) => {
   try {
     const { name, version } = req.body;
     const newProgram = await Program.create({ name, version });
@@ -19,7 +75,34 @@ exports.createProgram = async (req, res) => {
   }
 };
 
-exports.getProgramById = async (req, res) => {
+/**
+ * @swagger
+ * /api/programs/{id}:
+ *   get:
+ *     summary: Buscar programa por ID
+ *     description: Retorna um programa específico pelo seu ID
+ *     tags: [Programs]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID único do programa
+ *         example: 1
+ *     responses:
+ *       200:
+ *         description: Programa encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Program'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
+export const getProgramById = async (req, res) => {
   try {
     const program = await Program.findByPk(req.params.id);
     if (!program) {
@@ -31,7 +114,53 @@ exports.getProgramById = async (req, res) => {
   }
 };
 
-exports.updateProgram = async (req, res) => {
+/**
+ * @swagger
+ * /api/programs/{id}:
+ *   put:
+ *     summary: Atualizar um programa
+ *     description: Atualiza as informações de um programa existente
+ *     tags: [Programs]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID único do programa
+ *         example: 1
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 maxLength: 100
+ *                 description: Nome do programa
+ *                 example: Visual Studio Code
+ *               version:
+ *                 type: string
+ *                 maxLength: 50
+ *                 description: Versão do programa
+ *                 example: 1.85.0
+ *     responses:
+ *       200:
+ *         description: Programa atualizado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Program'
+ *       400:
+ *         $ref: '#/components/responses/BadRequest'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
+export const updateProgram = async (req, res) => {
   try {
     const { name, version } = req.body;
     const program = await Program.findByPk(req.params.id);
@@ -45,7 +174,38 @@ exports.updateProgram = async (req, res) => {
   }
 };
 
-exports.deleteProgram = async (req, res) => {
+/**
+ * @swagger
+ * /api/programs/{id}:
+ *   delete:
+ *     summary: Excluir um programa
+ *     description: Remove um programa do sistema
+ *     tags: [Programs]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID único do programa
+ *         example: 1
+ *     responses:
+ *       200:
+ *         description: Programa excluído com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Program deleted
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
+export const deleteProgram = async (req, res) => {
   try {
     const program = await Program.findByPk(req.params.id);
     if (!program) {
