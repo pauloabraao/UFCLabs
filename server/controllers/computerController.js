f48ce76ef122be5edfc66905475dc2448c7069a2
igcozmu <igoremanuel28@gmail.com>
2025-07-24 15:28:00 -0300

    Update computerController

--- server/controllers/computerController.js
+++ server/controllers/computerController.js
@@ -1,6 +1,32 @@
-const Computer = require('../models/Computer');
+import Computer from '../models/Computer.js';
 
-exports.getAllComputers = async (req, res) => {
+/**
+ * @swagger
+ * /api/computers:
+ *   get:
+ *     summary: Listar todos os computadores
+ *     description: Retorna uma lista com todos os computadores. Pode filtrar por lab_id através de query parameter.
+ *     tags: [Computers]
+ *     parameters:
+ *       - in: query
+ *         name: lab_id
+ *         schema:
+ *           type: integer
+ *         description: ID do laboratório para filtrar os computadores
+ *         example: 1
+ *     responses:
+ *       200:
+ *         description: Lista de computadores recuperada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Computer'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
+export const getAllComputers = async (req, res) => {
   try {
     const where = {};
     if (req.query.lab_id) {
@@ -13,7 +39,63 @@ exports.getAllComputers = async (req, res) => {
   }
 };
 
-exports.createComputer = async (req, res) => {
+/**
+ * @swagger
+ * /api/computers:
+ *   post:
+ *     summary: Criar um novo computador
+ *     description: Adiciona um novo computador ao sistema
+ *     tags: [Computers]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required: [lab_id, status]
+ *             properties:
+ *               lab_id:
+ *                 type: integer
+ *                 description: ID do laboratório
+ *                 example: 1
+ *               os:
+ *                 type: string
+ *                 maxLength: 100
+ *                 description: Sistema operacional
+ *                 example: Ubuntu 22.04 LTS
+ *               cpu:
+ *                 type: string
+ *                 maxLength: 100
+ *                 description: Processador
+ *                 example: Intel Core i5-10400
+ *               ram:
+ *                 type: string
+ *                 maxLength: 50
+ *                 description: Memória RAM
+ *                 example: 8GB DDR4
+ *               storage:
+ *                 type: string
+ *                 maxLength: 50
+ *                 description: Armazenamento
+ *                 example: 256GB SSD
+ *               status:
+ *                 type: string
+ *                 enum: [disponivel, fora de servico, em reparo]
+ *                 description: Status do computador
+ *                 example: disponivel
+ *     responses:
+ *       201:
+ *         description: Computador criado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Computer'
+ *       400:
+ *         $ref: '#/components/responses/BadRequest'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
+export const createComputer = async (req, res) => {
   try {
     const { lab_id, number_id, os, cpu, ram, storage, status } = req.body;
     const newComputer = await Computer.create({ lab_id, number_id, os, cpu, ram, storage, status });
@@ -23,7 +105,34 @@ exports.createComputer = async (req, res) => {
   }
 };
 
-exports.getComputerById = async (req, res) => {
+/**
+ * @swagger
+ * /api/computers/{id}:
+ *   get:
+ *     summary: Buscar computador por ID
+ *     description: Retorna um computador específico pelo seu ID
+ *     tags: [Computers]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID único do computador
+ *         example: 1
+ *     responses:
+ *       200:
+ *         description: Computador encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Computer'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
+export const getComputerById = async (req, res) => {
   try {
     const computer = await Computer.findByPk(req.params.id);
     if (!computer) {
@@ -35,7 +144,72 @@ exports.getComputerById = async (req, res) => {
   }
 };
 
-exports.updateComputer = async (req, res) => {
+/**
+ * @swagger
+ * /api/computers/{id}:
+ *   put:
+ *     summary: Atualizar um computador
+ *     description: Atualiza as informações de um computador existente
+ *     tags: [Computers]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID único do computador
+ *         example: 1
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               lab_id:
+ *                 type: integer
+ *                 description: ID do laboratório
+ *                 example: 1
+ *               os:
+ *                 type: string
+ *                 maxLength: 100
+ *                 description: Sistema operacional
+ *                 example: Ubuntu 22.04 LTS
+ *               cpu:
+ *                 type: string
+ *                 maxLength: 100
+ *                 description: Processador
+ *                 example: Intel Core i5-10400
+ *               ram:
+ *                 type: string
+ *                 maxLength: 50
+ *                 description: Memória RAM
+ *                 example: 8GB DDR4
+ *               storage:
+ *                 type: string
+ *                 maxLength: 50
+ *                 description: Armazenamento
+ *                 example: 256GB SSD
+ *               status:
+ *                 type: string
+ *                 enum: [disponivel, fora de servico, em reparo]
+ *                 description: Status do computador
+ *                 example: disponivel
+ *     responses:
+ *       200:
+ *         description: Computador atualizado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Computer'
+ *       400:
+ *         $ref: '#/components/responses/BadRequest'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
+export const updateComputer = async (req, res) => {
   try {
     const { lab_id, number_id, os, cpu, ram, storage, status } = req.body;
     const computer = await Computer.findByPk(req.params.id);
@@ -49,7 +223,38 @@ exports.updateComputer = async (req, res) => {
   }
 };
 
-exports.deleteComputer = async (req, res) => {
+/**
+ * @swagger
+ * /api/computers/{id}:
+ *   delete:
+ *     summary: Excluir um computador
+ *     description: Remove um computador do sistema
+ *     tags: [Computers]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID único do computador
+ *         example: 1
+ *     responses:
+ *       200:
+ *         description: Computador excluído com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Computer deleted
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
+export const deleteComputer = async (req, res) => {
   try {
     const computer = await Computer.findByPk(req.params.id);
     if (!computer) {
